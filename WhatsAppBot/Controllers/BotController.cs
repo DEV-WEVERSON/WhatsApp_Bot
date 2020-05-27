edd7f535e99f4025104fcc44f9d767caf9267a80
Márcio Praxedes <marcio.praxedes@hotmail.com.br>
2020-05-27 11:58:03 -0300

    updade code

--- WhatsAppBot/Controllers/BotController.cs
+++ WhatsAppBot/Controllers/BotController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Xml.XPath;
 using Microsoft.AspNetCore.Mvc;
 using OpenQA.Selenium;
@@ -8,23 +9,24 @@ namespace WhatsAppBot.Controllers
 {
     public class BotController : Controller
     {
-        private readonly IWebDriver _driver = new ChromeDriver();
+        private static readonly IWebDriver _driver = new ChromeDriver();
 
-        private const string Contatos = "'//*[@id=\"main\"]/header/div[2]/div[2]/span'";
+        private const string Contatos = "//*[@id=\"main\"]/header/div[2]/div[2]/span";
         private const string BtnEnviar = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button";
-        private const string ProcurarContato = "'//*[@id=\"app\"]/div/div/div[2]/div[1]/span/div/span/div/div[1]/div/label/div/div[2]'";
         private const string MensagemBox = "//*[@id=\"main\"]/footer/div[1]/div[2]/div/div[2]";
-        private const string PrimeiroContato = "'//*[@id=\"app\"]/div/div/div[2]/div[1]/span/div/span/div/div[2]/div/div/div/div[2]/div'";
-        private const string NovoChat = "'//*[@id=\"side\"]/header/div[2]/div/span/div[2]/div'";
-        
-        
+        private const string PrimeiroContato = "//*[@id=\"pane-side\"]/div[1]/div/div/div[1]/div";
+        private const string NovoChat = "//*[@id=\"side\"]/div[1]/div/label/div/div[2]";
+        private const string MensagemNaoLida = "//*[@id=\"pane-side\"]/div[1]/div/div/div[15]/div/div/div[2]/div[2]/div[2]/span[1]/div[2]/span";
+        private const string ListaConversas = "//*[@id=\"pane-side\"]/div[1]/div/div";
+        private const string Conversa = "//*[@id=\"pane-side\"]/div[1]/div/div/div[2]";
+        private const string NomeContato = "//*[@id=\"pane-side\"]/div[1]/div/div/div[11]/div/div/div[2]/div[1]/div[1]/span";
+
+       
         // GET
         public IActionResult IniciarBot()
         {
             
             _driver.Navigate().GoToUrl("https://web.whatsapp.com");
-            
-            EnviarMensagem("teste 123");
             return View("Index");
         }
 
@@ -62,11 +64,51 @@ namespace WhatsAppBot.Controllers
             var inputMensagem = GetElement(MensagemBox);
             inputMensagem.SendKeys(mensagem);
         }
-        [HttpPost]
+        
         public void EnviarMensagem(string mensagem)
         {
             EscreverMensagem(mensagem);
             Click();
+        }
+
+        public async void ProcurarContato(string nomeContato)
+        {
+            var novoChat = _driver.FindElement(By.XPath(NovoChat));
+            novoChat.Clear();
+            novoChat.Click();
+            novoChat.SendKeys(nomeContato);
+            
+            await Task.Delay(0700);
+
+            try
+            {
+                var contatoProcurado = _driver.FindElement(By.XPath(PrimeiroContato));
+                contatoProcurado.Click();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Não encontrado");
+            }
+            
+        }
+
+        public void EscutarConversas()
+        {
+            var listaConversas = _driver.FindElement(By.XPath(ListaConversas));
+
+            var conversas = listaConversas.FindElements(By.XPath(Conversa));
+
+            foreach (var conversa in conversas)
+            {
+                if (conversa.FindElement(By.XPath(MensagemNaoLida)) != null)
+                {
+                    var nomeContato = conversa.FindElement(By.XPath(NomeContato)).Text;
+                    ProcurarContato(nomeContato);
+                    EnviarMensagem("Olá. Mensagem de teste");
+                }
+                
+            }
+            
         }
     }
 }
